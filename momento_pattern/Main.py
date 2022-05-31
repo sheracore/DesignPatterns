9d27326697f067352f2b8de86c45ab7bad628697
sheracore <m.ghaffari662@gmail.com>
2022-05-31 10:02:40 +0430

    Add new pic for iterator_pattern

--- momento_pattern/Main.py
+++ momento_pattern/Main.py
@@ -1,16 +1,18 @@
 from Editor import Editor
 from Histoty import History
 
-editor = Editor()
-history = History()
 
-editor.content = 'a'
-history.push(editor.create_state())
+if __name__ == '__main__':
+    editor = Editor()
+    history = History()
 
-editor.content = 'c'
-history.push(editor.create_state())
+    editor.content = 'a'
+    history.push(editor.create_state())
 
-editor.content = 'd'
-editor.restore(history.pop())
+    editor.content = 'c'
+    history.push(editor.create_state())
 
-print(editor.content)
+    editor.content = 'd'
+    editor.restore(history.pop())
+
+    print(editor.content)
