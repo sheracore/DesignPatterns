f7d13a3682bead860f2695aed10986ac49cecbb7
sheracore <m.ghaffari662@gmail.com>
2022-06-03 19:52:57 +0430

    Add final command pattern

--- command_pattern/simple_command.py
+++ command_pattern/simple_command.py
@@ -7,7 +7,7 @@ class SimpleCommand(Command):
     """
 
     def __init__(self, payload: str) -> None:
-        pass
+        self._payload = payload
 
     def execute(self) -> None:
         print(f"SimpleCommand: See, I can do simple things like printing"
